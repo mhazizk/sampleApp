1ad73470993a1ca9b73c630f9c185785342f739c
mhazizk <mhazizk@gmail.com>
2023-04-16 23:14:39 +0700

    Initial Commit

---
+++ sampleApp/sampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  sampleAppApp.swift
+//  sampleApp
+//
+//  Created by Haziz on 16/04/23.
+//
+
+import SwiftUI
+
+@main
+struct sampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

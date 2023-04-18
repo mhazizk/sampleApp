6bc7d5c183f53a389b174bfe32e39333dc9b1ed1
mhazizk <mhazizk@gmail.com>
2023-04-19 00:12:25 +0700

    Create FavoriteScreen.swift

---
+++ sampleApp/screens/FavoriteScreen.swift
@@ -0,0 +1,24 @@
+//
+//  ProductsScreen.swift
+//  sampleApp
+//
+//  Created by Haziz on 18/04/23.
+//
+
+import Foundation
+import SwiftUI
+
+
+struct FavoriteScreen : View {
+    var body: some View {
+        VStack{
+            Text("Favorite")
+        }
+    }
+}
+
+struct FavoriteScreen_Previews : PreviewProvider {
+    static var previews: some View {
+        FavoriteScreen()
+    }
+}

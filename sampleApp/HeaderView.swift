16ce3096e35e3b025d7fc340fab8bb09aa87c66c
mhazizk <mhazizk@gmail.com>
2023-04-18 16:23:53 +0700

    Create HeaderView.swift

---
+++ sampleApp/HeaderView.swift
@@ -0,0 +1,17 @@
+//
+//  HeaderView.swift
+//  sampleApp
+//
+//  Created by Haziz on 16/04/23.
+//
+
+import SwiftUI
+
+struct HeaderView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+            Text("Header")
+        }
+    }
+}
